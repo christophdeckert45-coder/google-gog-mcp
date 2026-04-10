bf132a6469e12016f3fdd41b0575aa817e6925bc
Chris Deckert <christoph.deckert45@gmail.com>
2026-04-10 22:44:05 +0200

    Align api/debug.ts with no-underscore Google env vars

--- api/debug.ts
+++ api/debug.ts
@@ -1,7 +1,17 @@
 import type { IncomingMessage, ServerResponse } from "http";
 
 export default function handler(_req: IncomingMessage, res: ServerResponse) {
+  const envKeys = Object.keys(process.env).sort();
   res.statusCode = 200;
   res.setHeader("content-type", "application/json; charset=utf-8");
-  res.end(JSON.stringify(Object.keys(process.env)));
+  res.end(
+    JSON.stringify({
+      keys: envKeys,
+      truthy: {
+        GOOGLECLIENTID: Boolean(process.env.GOOGLECLIENTID),
+        GOOGLECLIENTSECRET: Boolean(process.env.GOOGLECLIENTSECRET),
+        GOOGLEREFRESHTOKEN: Boolean(process.env.GOOGLEREFRESHTOKEN),
+      },
+    }),
+  );
 }
